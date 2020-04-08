fc3a89d26cbb53b76b3750f442df34572288d991
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-08 01:00:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,8 @@ def find_even_values(src)
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  while element_index < spice_rack[row_index].count do
-    p spice_rack[row_index][element_index]
+  while element_index < src[row_index].count do
+    p src[row_index][element_index].even
     element_index += 1
   end
   row_index += 1
