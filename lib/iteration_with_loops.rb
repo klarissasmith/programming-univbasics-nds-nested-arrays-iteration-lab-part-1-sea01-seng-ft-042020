07835aac9be23f46883733ec0781a1b1dfb8dc6e
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-08 01:12:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     # p src[row_index][element_index].even?
-    if src[row_index][element_index].even? == true
+    if src[row_index][element_index].even? == 'true'
   new_array << src[row_index][element_index]
   
   element_index += 1
